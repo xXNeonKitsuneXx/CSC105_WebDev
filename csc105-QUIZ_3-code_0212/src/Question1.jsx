1836b07a49786930846f07a76e29d130e1dfef63
xXNeonKitsuneXx <xXNeonKitsuneXx@gmail.com>
2023-03-30 11:22:48 +0700

    Float fix

--- csc105-QUIZ_3-code_0212/src/Question1.jsx
+++ csc105-QUIZ_3-code_0212/src/Question1.jsx
@@ -15,14 +15,18 @@ import {
 // - PLEASE LOOK AT ALL PROPS THAT USED IN Password AND Verification 
 const Question = () => {
     const [step,setStep] = useState(0)
-    const [positionWidth,setPositionHeight] = useState(0)
+    const [positionHeight,setPositionHeight] = useState(0)
+    const [positionWidth,setPositionWidth] = useState(0)
 
   return (
     <Box display={"flex"} flexDirection="column" width={"100vw"} height={"100vh"} justifyContent={"center"} alignItems={"center"}>
       {step === 0 && <Password setStep={setStep}/> }
       {step === 1 && <Verification 
-      positionWidth={setPositionHeight} 
-      setPositionHeight={positionWidth} />}
+      setStep={setStep}
+      positionWidth={positionWidth} 
+      setPositionWidth={setPositionWidth}
+      positionHeight={positionHeight}
+      setPositionHeight={setPositionHeight} />}
       {step === 2 && <Party/>}
       
     </Box>
